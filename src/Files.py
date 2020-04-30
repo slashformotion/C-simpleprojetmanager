2640da3c448e1acebda8d5e407210cc58d4e28c1
slashformotion <theophile.roos@gmail.com>
2020-04-30 12:02:54 +0200

    Updated : ui files and Files.py

--- src/Files.py
+++ src/Files.py
@@ -6,133 +6,140 @@ import os
 import getpass
 
 
+class MetaFile(type):
+    def __new__(cls, name, bases, body):
+        """
+        Me
+        """
+        print(name,bases, body)
+        if not 'EXTENSION' in body:
+            raise NotImplementedError("EXTENSION class attr is not declared")
+        else:
+            if not isinstance(body['EXTENSION'],str):
+                raise TypeError("Wrong type for \"EXTENSION\" class attr is not declared")
 
-class Module():
+        return super().__new__(cls, name, bases, body)
 
-    def __init__(self, det): # det is modulename::namespacename
-        assert isinstance(det, str)
+class File(metaclass = MetaFile):
+    """
+    sub class : define file 
 
-        self.namespace = None
-        self.class_name = None
-        if len(det.split("::"))==1:
-                self.module_name = det.split("::")[0]
-        elif len(det.split("::"))==2:
-            self.module_name = det.split("::")[0]
-            self.namespace = det.split("::")[1].lower()
-        elif len(det.split("::"))==3:
-            self.module_name = det.split("::")[0]
-            self.namespace = det.split("::")[1].lower()
-            self.class_name = det.split("::")[2].title()
+    Args:
+        metaclass (MetaFile), optional): . Defaults to MetaFile.
+    """
+    def __init__(self, name):
+        assert isinstance(name, str)
+        self.name = name
 
-    def exists(self, path_init):
-        path_hpp = path_init / Path(self.module_name + ".hpp")
-        path_cpp = path_init / Path(self.module_name + ".cpp")
-        if path_hpp.exists():
-            print(f"The file {self.module_name}.hpp already exists, aborting...")
-            os.abort()
-        if path_cpp.exists():
-            print(f"The file {self.module_name}.cpp already exists, aborting...")
-            os.abort()
-
-
-    def deploy(self, path_init):
-        ################ HPP ###############
-
-        path = path_init / Path(self.module_name + ".hpp")
-        with path.open("w") as f:
-            if self.namespace != None:
-                id = "_".join([self.module_name.upper(),self.namespace.upper(),"HPP"])
-            else:
-                id = "_".join([self.module_name.upper(),"HPP"])
+    @property
+    def reduced_path(self):
+        return Path("".join([self.name, self.EXTENSION]))
 
 
-            f.write(f"#ifndef {id}\n#define {id}\n\n\n\n")
+class Main(File):
+    
+    EXTENSION = "cpp"
+    def __init__(self, name):
+        super().__init__(name)
+        self.headers_names = []
 
-            if self.namespace != None:
-                f.write(f"namespace {self.namespace}\n@\n    \n".replace("@", "{"))
+    def add_header(self, header_name):
+        """
+        add header_name argument to headers_names attribut list 
 
-                if self.class_name !=None:
-                    f.write(f"    class {self.class_name}\n    @\n        public:\n\n        private:\n\n".replace("@", "{"))
-                    f.write(f"    @; // class {self.class_name}\n\n".replace("@", "}"))
-                f.write(f"@ // namespace {self.namespace}\n\n\n".replace("@", "}"))
-            f.write(f"#endif // {id}")
+        Args:
+            header_name (str): header_name to include
 
-
-        ################ CPP ###############
-        path = path_init / Path(self.module_name + ".cpp")
-        with path.open("w") as f:
-            f.write(f"#include \"{self.module_name}.hpp\"\n")
-
-            if self.namespace != None:
-                f.write(f"namespace {self.namespace}\n@\n    \n".replace("@", "{"))
-                f.write(f"@ // namespace {self.namespace}\n\n\n".replace("@", "}"))
-
-    def get_header_include(self):
-        return f"#include \"{self.module_name}.hpp\""
-
-
-
-
-
-
-
-
-
-
-
-class Main_File:
-    def __init__(self, path):
+        Raises:
+            TypeError: Error on the argument type
+        """
+        try:
+            if not isinstance(header_name, str):
+                raise TypeError("Wrong type for <header_name> argument in Main.add_header() function")
+        except TypeError as e:
+            print(e)
+        else:
+            self.headers_names.append(header_name)
         
-        self.path = path
-        self.headers_includes = []
+    def __get_includes(self):
+        """
+        return a single string containing all of the includes for the deploy function
 
-    def add_header_include(self, include):
-        assert type(include) is str
-        assert include.startswith("#include ")
-        self.headers_includes.append(include)
+        Returns:
+            str: all of the includes we need
+        """
+        str_includes = ""
 
+        for hn in self.headers_names:
+            str_includes += f"#include \"{hn}.{self.EXTENSION}\"\n" 
 
+        return str_includes
+        
+    def __get_date(self):
+        """
+        return the date
 
-    def deploy_at_init(self,PATH):
+        Returns:
+            datetime.datetime: current date of the detected timezone
+        """
+        return det.datetime.now(tz.tzlocal())
 
-        time = det.datetime.now(tz.tzlocal())
-        with self.path.open("w") as f:
-            f.write(f"// File : {self.path.name}\n")
+    def __deploy_on_create(self,MAIN_FILE):
+        """
+        deploy main file when it doesn't exist
 
+        Args:
+            WORKSPACE (pathlib.Path): path to workspace
+        """
+        time = self.__get_date()
+        with MAIN_FILE.open("w") as f:
+            #PRESENTATION HEADER
+            f.write(f"// File : {self.reduced_path.name}\n")
             f.write("// Created : " + time.strftime("%A %d %B") + " at " + time.strftime("%H") + "h - " + time.strftime("%M") + "min\n")
-            f.write(f"// user : {getpass.getuser()}")
-            f.write(f"\n\n//// PERSONAL MODULES\n")
+            f.write(f"// user : {getpass.getuser()}\n")
+            # INCLUDE INSTRUCTION AND MAIN FUNC
+            f.write(f"\n//// PERSONAL MODULES\n")
+            f.write(self.__get_includes())
+            f.write("\n\n//// BUILTIN LIBS\n#include <iostream>\n\n\n\nint main(){\n\n     return 0;\n}\n")   
 
-            if len(self.headers_includes)!=0:
-                for include in self.headers_includes:
-                    f.write(f"{include}\n")
-            f.write("\n\n\n//// BUILTIN LIBS\n#include <iostream>\n\n\n\nint main(){\n\n     return 0;\n}\n")      
-
-    def deploy_at_runtime(self,PATH):
+    def __deploy_on_edit(self,MAIN_FILE):
         raw_data = None
-        with self.path.open("r") as f:
+        with MAIN_FILE.open("r") as f:
             raw_data = f.read()
         sep = "//// PERSONAL MODULES\n"
         if sep in raw_data:
             raw_data_splited = raw_data.split(sep)
-            start, end = raw_data_splited[0], raw_data_splited[1]
-            new_includes = str()
-            for include in self.headers_includes:
-                new_includes+= include + "\n"
-            new_raw_data = start + sep+new_includes+end
-            with path.open("w") as f:
+            del raw_data
+            start, end = raw_data_splited
+            new_includes = self.__get_includes()   
+            
+            new_raw_data = start + sep + new_includes + end
+            with MAIN_FILE.open("w") as f:
                 f.write(new_raw_data)
         else:
-            for include in self.headers_includes:
-                new_includes+= include + "\n"
-            with path.open("w") as f:
+            with MAIN_FILE.open("w") as f:
 
-                f.write(new_includes + raw_data)
+                f.write(self.__get_includes() + raw_data)
+        
+    def deploy(self,WORKSPACE):
+        if not WORKSPACE.is_dir():
+            raise TypeError("Wrong type for <WORKSPACE> argument")
+        
+        MAIN_FILE = WORKSPACE / self.reduced_path
+        if not MAIN_FILE.exists():
+            self.__deploy_on_create(MAIN_FILE)
+        else:
+            self.__deploy_on_edit(MAIN_FILE)
+
+
+        
+        
+   
+    
 
 
 
-    def exists(self, PATH):
-        return self.get_path(PATH).exists()
 
-    def get_path(self, PATH):
-        return self.path
+
+    
+
