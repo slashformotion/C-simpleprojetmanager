455c7fc6da4fe1e12957475723395a99e70fe8c9
slashformotion <theophile.roos@gmail.com>
2020-05-13 16:10:40 +0200

    refacto 1

--- old/Files.py
+++ old/Files.py
@@ -5,136 +5,210 @@ import dateutil.tz as tz
 import os
 import getpass
 
+from utils import str_handling as sh
 
 
-class Module():
+class MetaFile(type):
+    def __new__(cls, name, bases, body):
+        """
+        Me
+        """
+        # print(name,bases, body)
+        if not 'EXTENSION' in body:
+            raise NotImplementedError("EXTENSION class attr is not declared")
+        else:
+            if not isinstance(body['EXTENSION'],str):
+                raise TypeError("Wrong type for \"EXTENSION\" class attr is not declared")
 
-    def __init__(self, det): # det is modulename::namespacename
-        assert isinstance(det, str)
+        return super().__new__(cls, name, bases, body)
 
-        self.namespace = None
-        self.class_name = None
-        if len(det.split("::"))==1:
-                self.module_name = det.split("::")[0]
-        elif len(det.split("::"))==2:
-            self.module_name = det.split("::")[0]
-            self.namespace = det.split("::")[1].lower()
-        elif len(det.split("::"))==3:
-            self.module_name = det.split("::")[0]
-            self.namespace = det.split("::")[1].lower()
-            self.class_name = det.split("::")[2].title()
+class File(metaclass = MetaFile):
+    """
+    sub class : define file 
 
-    def exists(self, path_init):
-        path_hpp = path_init / Path(self.module_name + ".hpp")
-        path_cpp = path_init / Path(self.module_name + ".cpp")
-        if path_hpp.exists():
-            print(f"The file {self.module_name}.hpp already exists, aborting...")
-            os.abort()
-        if path_cpp.exists():
-            print(f"The file {self.module_name}.cpp already exists, aborting...")
-            os.abort()
+    Args:
+        metaclass (MetaFile), optional): . Defaults to MetaFile.
+    """
+    EXTENSION = ''
+    def __init__(self, name):
+        assert isinstance(name, str)
+        print(name)
+        assert len(name)>=3
+        self.name = name
 
-
-    def deploy(self, path_init):
-        ################ HPP ###############
-
-        path = path_init / Path(self.module_name + ".hpp")
-        with path.open("w") as f:
-            if self.namespace != None:
-                id = "_".join([self.module_name.upper(),self.namespace.upper(),"HPP"])
-            else:
-                id = "_".join([self.module_name.upper(),"HPP"])
+    def exists(self, WORKSPACE):
+        assert WORKSPACE.is_dir()
+        assert WORKSPACE.exists()
+        return (WORKSPACE / self.reduced_path)
 
 
-            f.write(f"#ifndef {id}\n#define {id}\n\n\n\n")
-
-            if self.namespace != None:
-                f.write(f"namespace {self.namespace}\n@\n    \n".replace("@", "{"))
-
-                if self.class_name !=None:
-                    f.write(f"    class {self.class_name}\n    @\n        public:\n\n        private:\n\n".replace("@", "{"))
-                    f.write(f"    @; // class {self.class_name}\n\n".replace("@", "}"))
-                f.write(f"@ // namespace {self.namespace}\n\n\n".replace("@", "}"))
-            f.write(f"#endif // {id}")
+    @property
+    def reduced_path(self):
+        return Path(".".join([self.name, self.EXTENSION]))
 
 
-        ################ CPP ###############
-        path = path_init / Path(self.module_name + ".cpp")
-        with path.open("w") as f:
-            f.write(f"#include \"{self.module_name}.hpp\"\n")
+class Main(File):
+    
+    EXTENSION = "cpp"
+    def __init__(self, name):
+        super().__init__(name)
+        self.headers_names = []
 
-            if self.namespace != None:
-                f.write(f"namespace {self.namespace}\n@\n    \n".replace("@", "{"))
-                f.write(f"@ // namespace {self.namespace}\n\n\n".replace("@", "}"))
+    def add_header(self, header_name):
+        """
+        add header_name argument to headers_names attribut list 
 
-    def get_header_include(self):
-        return f"#include \"{self.module_name}.hpp\""
+        Args:
+            header_name (str): header_name to include
 
+        Raises:
+            TypeError: Error on the argument type
+        """
+        try:
+            if not isinstance(header_name, str):
+                raise TypeError("Wrong type for <header_name> argument in Main.add_header() function")
+        except TypeError as e:
+            print(e)
+        else:
+            self.headers_names.append(header_name)
+        
+    def __get_includes(self):
+        """
+        return a single string containing all of the includes for the deploy function
 
+        Returns:
+            str: all of the includes we need
+        """
+        str_includes = ""
 
+        for hn in self.headers_names:
+            str_includes += f"#include \"{hn}\"\n" 
 
+        return str_includes
+        
+    def __get_date(self):
+        """
+        return the date
 
+        Returns:
+            datetime.datetime: current date of the detected timezone
+        """
+        return det.datetime.now(tz.tzlocal())
 
+    def __deploy_on_create(self,MAIN_FILE):
+        """
+        deploy main file when it doesn't exist
 
-
-
-
-
-class Main_File:
-    def __init__(self, det):
-        assert type(det) is str
-        self._name = det + ".cpp"
-        self.headers_includes = []
-
-    def add_header_include(self, include):
-        assert type(include) is str
-        assert include.startswith("#include ")
-        self.headers_includes.append(include)
-
-
-
-    def deploy_at_init(self,PATH):
-
-        time = det.datetime.now(tz.tzlocal())
-        path = PATH / Path(self._name)
-        with path.open("w") as f:
-            f.write(f"// File : {self._name}\n")
-
+        Args:
+            WORKSPACE (pathlib.Path): path to workspace
+        """
+        time = self.__get_date()
+        with MAIN_FILE.open("w") as f:
+            #PRESENTATION HEADER
+            f.write(f"// File : {self.reduced_path.name}\n")
             f.write("// Created : " + time.strftime("%A %d %B") + " at " + time.strftime("%H") + "h - " + time.strftime("%M") + "min\n")
-            f.write(f"// user : {getpass.getuser()}")
-            f.write(f"\n\n//// PERSONAL MODULES\n")
+            f.write(f"// user : {getpass.getuser()}\n")
+            # INCLUDE INSTRUCTION AND MAIN FUNC
+            f.write(f"\n//// PERSONAL MODULES\n")
+            f.write(self.__get_includes())
+            f.write("\n\n//// BUILTIN LIBS\n#include <iostream>\n\n\n\nint main(){\n\n     return 0;\n}\n")   
 
-            if len(self.headers_includes)!=0:
-                for include in self.headers_includes:
-                    f.write(f"{include}\n")
-            f.write("\n\n\n//// BUILTIN LIBS\n#include <iostream>\n\n\n\nint main(){\n\n     return 0;\n}\n")      
-
-    def deploy_at_runtime(self,PATH):
-        path = PATH / Path(self._name)
+    def __deploy_on_edit(self,MAIN_FILE):
         raw_data = None
-        with path.open("r") as f:
+        with MAIN_FILE.open("r") as f:
             raw_data = f.read()
         sep = "//// PERSONAL MODULES\n"
         if sep in raw_data:
             raw_data_splited = raw_data.split(sep)
-            start, end = raw_data_splited[0], raw_data_splited[1]
-            new_includes = str()
-            for include in self.headers_includes:
-                new_includes+= include + "\n"
-            new_raw_data = start + sep+new_includes+end
-            with path.open("w") as f:
+            del raw_data
+            start, end = raw_data_splited
+            new_includes = self.__get_includes()   
+            
+            new_raw_data = start + sep + new_includes + end
+            with MAIN_FILE.open("w") as f:
                 f.write(new_raw_data)
         else:
-            for include in self.headers_includes:
-                new_includes+= include + "\n"
-            with path.open("w") as f:
+            with MAIN_FILE.open("w") as f:
 
-                f.write(new_includes + raw_data)
+                f.write(self.__get_includes() + raw_data)
+        
+    def deploy(self,WORKSPACE):
+        if not WORKSPACE.is_dir():
+            raise TypeError("Wrong type for <WORKSPACE> argument")
+        
+        MAIN_FILE = WORKSPACE / self.reduced_path
+        if not MAIN_FILE.exists():  
+            self.__deploy_on_create(MAIN_FILE)
+        else:
+            self.__deploy_on_edit(MAIN_FILE)
+
+class Module(File):
+    EXTENSION = 'hpp'
+    
+    def __init__(self, name, namespace = "", klass = ""):
+        super().__init__(name)
+        if namespace == "" or sh.is_only_whitespaces(namespace) or sh.is_there_space_in_middle_str(namespace) or len(namespace)<3:
+            self.namespace = None
+            self.klass = None
+        else:
+            self.namespace = namespace.lower()
+            if klass == "" or sh.is_only_whitespaces(klass) or sh.is_there_space_in_middle_str(klass) or len(klass)<3:
+                self.klass = None
+            else:
+                self.klass = klass.title()
 
 
+    def send_header(self):
+        """
+        send the name of the module to the a Main instance
 
-    def exists(self, PATH):
-        return self.get_path(PATH).exists()
+        Returns:
+            str: name of the module
+        """
+        return ".".join([self.name,self.EXTENSION])
 
-    def get_path(self, PATH):
-        return PATH / Path(self._name)
+    def deploy(self, WORKSPACE):
+        ################ HPP ###############
+        hpp_file = WORKSPACE / self.reduced_path
+        with hpp_file.open("w") as f:
+            if self.namespace != None:
+                id_namespace = "_".join([self.name.upper(),self.namespace.upper(),self.EXTENSION.upper()])
+            else:
+                id_namespace = "_".join([self.name.upper(),self.EXTENSION.upper()])
+
+
+            f.write(f"#ifndef {id_namespace}\n#define {id_namespace}\n\n\n\n")
+
+            if self.namespace != None:
+                f.write(f"namespace {self.namespace}\n@\n    \n".replace("@", "{"))
+
+                if self.klass !=None:
+                    f.write(f"    class {self.klass}\n    @\n        public:\n\n        private:\n\n".replace("@", "{"))
+                    f.write(f"    @; // class {self.klass}\n\n".replace("@", "}"))
+                f.write(f"@ // namespace {self.namespace}\n\n\n".replace("@", "}"))
+            f.write(f"#endif // {id_namespace}")
+
+
+        ################ CPP ###############
+        path = WORKSPACE / Path(".".join([self.name, Main.EXTENSION]))
+        with path.open("w") as f:
+            f.write(f"#include \"{self.name}.{Main.EXTENSION}\"\n")
+
+            if self.namespace != None:
+                f.write(f"namespace {self.namespace}\n@\n    \n".replace("@", "{"))
+                f.write(f"@ // namespace {self.namespace}\n\n\n".replace("@", "}"))    
+        
+
+        
+    
+
+if __name__ == '__main__':
+    p = Path('.')
+    m = Main('prog')
+    hs = [Module("voila"), Module("TEST","truc", "Muche")]
+    for h in hs:
+        m.add_header(h.send_header())
+        h.deploy(p)
+    m.deploy(p, 1)
+    
+
